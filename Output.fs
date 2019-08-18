83b2f4b50db05c34815c61d32d17c599aea7d914
Dan Mannock <dmannock@users.noreply.github.com>
2019-08-18 21:29:54 +0100

    f# enum handcrafted ast to f# code play

--- Output.fs
+++ Output.fs
@@ -9,9 +9,16 @@ type Foo() =
     member x.Hi = "Hi!"
 """
 
+[<Literal>]
+let ExFsharpEnum = """module TopLevel
+type EnumTest =
+    | None = 0
+    | First = 1
+"""
+
 let makeIdent name = Ident(name, range.Zero)
 
-let myAst = 
+let ExFsharpAst = 
     let memberFlags : MemberFlags = {
         IsInstance = true
         IsDispatchSlot = false
@@ -126,8 +133,115 @@ let myAst =
     )    
     parsedImplFile |> ParsedInput.ImplFile
 
+let ExFsharpEnumAst = 
+    let componentInfo: SynComponentInfo = 
+        ComponentInfo(
+            (* SynAttributes *) 
+            SynAttributes.Empty,
+            (* SynTyparDecl list *) 
+            List.empty,
+            (* SynTypeConstraint list *) 
+            List.empty,
+            (* LongIdent *) 
+            [makeIdent "EnumTest"],
+            (* PreXmlDoc *) 
+            PreXmlDoc.Empty, 
+            (* PreferPostfix *) 
+            false,
+            (* SynAccess option *) 
+            None,
+            (* range *) 
+            range.Zero
+    )
+    let enumCases:SynEnumCase list = 
+        [
+            EnumCase(
+                (* SynAttributes *) 
+                SynAttributes.Empty,
+                (* ident:Ident *) 
+                makeIdent "None",
+                (* SynConst *) 
+                (SynConst.Int32 0),
+                (* PreXmlDoc *) 
+                PreXmlDoc.Empty,
+                (* range:range *) 
+                range.Zero
+            )
+            EnumCase(
+                (* SynAttributes *) 
+                SynAttributes.Empty,
+                (* ident:Ident *) 
+                makeIdent "First",
+                (* SynConst *) 
+                (SynConst.Int32 1),
+                (* PreXmlDoc *) 
+                PreXmlDoc.Empty,
+                (* range:range *) 
+                range.Zero
+            )
+        ]
+    let theEnum: SynTypeDefnSimpleRepr = 
+        SynTypeDefnSimpleRepr.Enum(
+            (* SynEnumCases *)
+            enumCases,
+            (* range *) 
+            range.Zero
+    )
+    let repr: SynTypeDefnRepr = 
+        SynTypeDefnRepr.Simple(theEnum, range.Zero)
+    let syncType: SynTypeDefn = 
+        TypeDefn(
+            (* SynComponentInfo *) 
+            componentInfo,
+            (* SynTypeDefnRepr *) 
+            repr,
+            (* SynMemberDefns *) 
+            List.empty,
+            (* range *) 
+            range.Zero
+    )
+    let modules = 
+        SynModuleOrNamespace(
+            (*longId:LongIdent *) 
+            [makeIdent "TopLevel"], //name
+            (* isRecursive:bool *) 
+            false,
+            (* isModule:bool *) 
+            true,
+            (* decls:SynModuleDecls *)  
+            [SynModuleDecl.Types([syncType], range.Zero)],
+            (* xmlDoc:PreXmlDoc *) 
+            PreXmlDoc.Empty,
+            (* attribs:SynAttributes *) 
+            SynAttributes.Empty,
+            (* accessibility:SynAccess option *) 
+            None,
+            (* range:range *)
+            range.Zero
+    )
+    let parsedImplFile = 
+        ParsedImplFileInput(
+            (*fileName*) 
+            "tmp.fsx",
+            (*isScript*) 
+            false,
+            (*qualifiedNameOfFile*) 
+            QualifiedNameOfFile(makeIdent "TopLevel"),
+            (*scopedPragmas: ScopedPragma list*) 
+            List.Empty,
+            (*hashDirectives : ParsedHashDirective list*) 
+            List.Empty, 
+            (*modules : SynModuleOrNamespace list*) 
+            [modules], 
+            ((* isLastCompiland  bool*) true, (* isExe  bool*) true)
+    )    
+    parsedImplFile |> ParsedInput.ImplFile
+
 let astTest() =
     let noOriginalSourceCode = "//"
     let config = { FormatConfig.FormatConfig.Default with StrictMode = true }
-    let myHandCraftedAst = CodeFormatter.FormatAST(myAst, noOriginalSourceCode, None, config)
+    // namespace with class, method & return type
+    // let myHandCraftedAst = CodeFormatter.FormatAST(ExFsharpAst, noOriginalSourceCode, None, config)
+    // enum ast example
+    let myHandCraftedAst = CodeFormatter.FormatAST(ExFsharpEnumAst, noOriginalSourceCode, None, config)
     myHandCraftedAst
